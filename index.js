5311cb803c71dfdabe69aff9a697d9537803ef62
Jee El <98971420+Jee-El@users.noreply.github.com>
2022-05-31 01:27:28 +0100

    Feat: Add basic operations' functionalities

--- index.js
+++ index.js
@@ -1,57 +1,127 @@
-const keys = document.querySelectorAll('.keys > button');
-const calculationDisplay = document.querySelector(`.calculation-display`);
+const operators = document.querySelectorAll('.operators:not(:first-child)');
+const clearBtn = document.querySelector(`.operators:first-child`);
+const equalBtn = operators[operators.length - 1];
+const numpad = document.querySelectorAll(`.numpad`);
+const calcDisplay = document.querySelector(`.calc-display`);
 
-keys.forEach((key) => {
-    if (key.classList.item(1) === `AC` || key.classList.item(1) === `=`) {
-        return;
-    }
-    key.addEventListener(`click`, (e) => {
-        calculationDisplay.textContent += `${e.target.classList.item(1)}`;
-    });
-})
+// AC key
+clearBtn.addEventListener(`click`, () => {
+	calcDisplay.textContent = ``;
+});
+
+let nums = [];
+// Show user input
+numpad.forEach((num) => {
+	num.addEventListener(`click`, (e) => {
+        if (calcDisplay.classList.contains(`test`)) {
+            calcDisplay.textContent = ``;
+            calcDisplay.classList.remove(`test`);
+        }
+		calcDisplay.textContent += e.target.textContent;
+	});
+});
+
+// Calculate user input
+let parameters = [];
+let pressedOperators = [];
+operators.forEach((operator) => {
+	operator.addEventListener(`click`, (e) => {
+		pressedOperators.push(e.target.textContent);
+		parameters.push(+calcDisplay.textContent);
+		calcDisplay.textContent = ``;
+		if (operator.textContent === `√`) {
+            calcDisplay.classList.add(`test`);
+			let result = getSquareRoot(...parameters);
+            calcDisplay.textContent = Math.round(result * 10 ** 7) / 10 ** 7;
+            parameters = [];
+		    pressedOperators = [];
+            return;
+		}
+		if (parameters.length === 1) return;
+        if (e.target.textContent === `=` && pressedOperators[0] !== `√`) {
+            calcDisplay.classList.add(`test`);
+			let result = operate(
+				...parameters,
+				pressedOperators[0]
+			);
+            calcDisplay.textContent = Math.round(result * (10 ** 7)) / 10 ** 7;
+            parameters = [];
+		    pressedOperators = [];
+            return;
+		}
+        calcDisplay.classList.add(`test`);
+        let result = operate(...parameters, pressedOperators[0])
+		calcDisplay.textContent = Math.round(result * 10 ** 7) / 10 ** 7;
+		parameters = [];
+		pressedOperators = [];
+	});
+});
 
 function add(a, b) {
-    return a + b;
+	return a + b;
 }
 
 function subtract(a, b) {
-    return a - b;
+	return a - b;
 }
 
 function multiply(a, b) {
-    return a * b;
+	return a * b;
 }
 
 function divide(a, b) {
-    return a / b;
+	return a / b;
 }
 
 function getPercentage(a, b) {
-    return multiply((parseInt(a)/100), b);
+	return multiply(a / 100, b);
 }
 
 function getSquareRoot(a) {
-    return Math.sqrt(a);
+	return Math.sqrt(a);
 }
 
-function operate(a, operator, b) {
-    switch (operator) {
-        case `+`:
-            add(a,b);
-            break;
-        case `-`:
-            subtract(a,b);
-            break;
-        case `x`:
-            multiply(a,b);
-            break;
-        case `÷`:
-            divide(a,b);
-            break;
-        case `%`:
-            getPercentage(a,b);
-            break;
-        case `√`:
-            getSquareRoot(a);
-    }
+// This one is only for GCD
+function checkForValidInput(a, b) {
+	if (
+		!Number.isInteger(a) ||
+		!Number.isInteger(b) ||
+		a.toString().split('').length > 12 ||
+		b.toString().split('').length > 12
+	) {
+		return (result.textContent = `Enter 12 digits or less.`);
+	}
+	if (a === 0 && b === 0) {
+		return (result.textContent = `One integer has to be non-null`);
+	}
+}
+function findGCD(a, b) {
+	a = Math.abs(a);
+	b = Math.abs(b);
+	if (a === 0) return b;
+	if (b === 0) return findGCD(b, a);
+	if (a % b === 0) {
+		return a > b ? b : a;
+	}
+	return findGCD(b, a % b);
+}
+
+function operate(a, b, operator) {
+	switch (operator) {
+		case `+`:
+			return add(a, b);
+		case `—`:
+			return subtract(a, b);
+		case `x`:
+			return multiply(a, b);
+			break;
+		case `÷`:
+			return divide(a, b);
+		case `%`:
+			return getPercentage(a, b);
+		case `√`:
+			return getSquareRoot(a);
+        case `GCD`:
+            return findGCD(a,b);
+	}
 }
