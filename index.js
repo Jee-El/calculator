a93ed00bcf73388e07611086d7b5fce011f36dfd
Jee El <98971420+Jee-El@users.noreply.github.com>
2022-06-03 00:09:51 +0100

    Fix: bug caused by pressing multiple operators
    This was only if they were pressed consecutively.
    This fixes some other minor bugs as well as adds comments to the code
    for clarity.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+const display = document.querySelector(`.display`);
+const numpad = document.querySelectorAll(`.numpad`);
 const operators = document.querySelectorAll('.operators:not(:first-child)');
 const clearBtn = document.querySelector(`.operators:first-child`);
 const equalBtn = operators[operators.length - 1];
-const numpad = document.querySelectorAll(`.numpad`);
-const calcDisplay = document.querySelector(`.calc-display`);
+const deleteBtn = document.querySelector(`.delete`);
 
 // Make x, +, = more noticeable
 operators[3].style.fontSize = `1.5rem`;
@@ -10,24 +11,35 @@ operators[5].style.fontSize = `1.5rem`;
 equalBtn.style.fontSize = `1.8rem`;
 
 // AC key
-clearBtn.addEventListener(`click`, () => {
-	calcDisplay.textContent = ``;
-	calcDisplay.classList.remove(`test`);
+clearBtn.addEventListener(`click`, clearData);
+function clearData(e) {
+	display.textContent = ``;
+	display.classList.remove(`test`);
 	para = [];
 	op = [];
+}
+
+// Delete button
+deleteBtn.addEventListener(`click`, () => {
+	if (display.textContent.length === 1) return (display.textContent = `0`);
+	display.textContent = display.textContent.slice(
+		0,
+		display.textContent.length - 1
+	);
 });
 
-let nums = [];
 // Show user input
-numpad.forEach((num) => {
-	num.addEventListener(`click`, (e) => {
-		if (calcDisplay.classList.contains(`result-is-displayed`)) {
-			calcDisplay.textContent = ``;
-			calcDisplay.classList.remove(`result-is-displayed`);
-		}
-		calcDisplay.textContent += e.target.textContent;
-	});
-});
+numpad.forEach((num) => num.addEventListener(`click`, getInput));
+function getInput(e) {
+	if (display.classList.contains(`result-is-displayed`)) {
+		display.textContent = ``;
+		display.classList.remove(`result-is-displayed`);
+	}
+	if (display.textContent.includes(`.`) && e.target.textContent === `.`) {
+		return;
+	}
+	display.textContent += e.target.textContent;
+}
 
 let para = [];
 let op = [];
@@ -35,34 +47,55 @@ let op = [];
 // Calculate user input
 operators.forEach((operator) => {
 	operator.addEventListener(`click`, (e) => {
-		if (calcDisplay.textContent.trim() === ``) {
-			para.push(0);
-		} else {
-			para.push(+calcDisplay.textContent);
+		if (
+			display.textContent.trim() === `` ||
+			display.textContent.trim() === ``
+		) {
+			if (op.length) {
+				// To change the operator to the latest clicked one if two or more are pressed one after another
+				op.shift();
+				op.push(e.target.textContent);
+				return;
+			}
+			// Otherwise if input is empty and no operator was clicked, leave
+			// The exception is `=`
+			return;
 		}
-		op.push(e.currentTarget.textContent);
-		calcDisplay.textContent = ``;
-
+		if (display.classList.contains(`result-is-displayed`)) {
+			display.textContent = ``;
+			display.classList.remove(`result-is-displayed`);
+		}
+		para.push(+display.textContent);
+		op.push(e.target.textContent);
+		display.textContent = ``;
+		// The square root doesn't require two parameters
 		if (e.currentTarget.textContent === `√`) {
-			calcDisplay.classList.add(`test`);
+			display.classList.add(`test`);
 			let result = getSquareRoot(para[0]);
-			calcDisplay.textContent = Math.round(result * 10 ** 7) / 10 ** 7;
+			display.textContent = Math.round(result * 10 ** 7) / 10 ** 7;
 			para = [];
 			op = [];
 			return;
 		}
 
+		// Need 2 parameters otherwise
 		if (para.length === 1) return;
-		if (op[0] === `=`) return op.shift();
-		if (op[1] !== `=`) {
-			op.shift();
-			op.push(`=`);
+		if (op[0] === `=`) op.shift();
+		let result = operate(para[0], para[1], op[0]);
+
+		// Result can be a string in case of an error, so Math.round wouldn't work
+		if (typeof result === `number`) {
+			display.textContent = Math.round(result * 10 ** 7) / 10 ** 7;
+		} else {
+			display.textContent = result;
 		}
-		let result = operate(para[para.length - 2], para[para.length - 1], op[0]);
+		display.classList.add(`result-is-displayed`);
+
+		// Save the result for next operation, clear the rest
 		para.splice(0, 2, result);
+
+		// Update the operator used in operate()
 		op.shift();
-		calcDisplay.classList.add(`result-is-displayed`);
-		return (calcDisplay.textContent = Math.round(result * 10 ** 7) / 10 ** 7);
 	});
 });
 
@@ -79,6 +112,7 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
+	if (b === 0) return `Error`;
 	return a / b;
 }
 
@@ -87,6 +121,7 @@ function getPercentage(a, b) {
 }
 
 function getSquareRoot(a) {
+	if (a < 0) return `Error`;
 	return Math.sqrt(a);
 }
 
